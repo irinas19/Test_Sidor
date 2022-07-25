0fdc64de7475a85db6c897a3b1f7fe6e7f6f4695
irinas <irina.sidor.2000@mail.ru>
2022-07-25 19:25:49 +0300

    craete array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array;
+int count; 
+string s; 
+
+Console.Write("count = ");
+count = Int32.Parse(Console.ReadLine());
+
+array = new string[count];
