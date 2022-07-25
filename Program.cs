2b74697e45763e46d81f139511a13504c1210200
irinas <irina.sidor.2000@mail.ru>
2022-07-25 19:46:15 +0300

    enter array

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@ Console.Write("count = ");
 count = Int32.Parse(Console.ReadLine());
 
 array = new string[count];
+
+Console.WriteLine("Enter array:");
+
+      for (int i=0; i<array.Length; i++)
+      {
+        Console.Write("array[{0}] = ", i);
+        array[i] = Console.ReadLine();
+      }
